06a7128c63d5e4cd788f2f437fb3c0a7fc226662
Ameer <ameerapex496@gmail.com>
2024-01-13 07:07:38 +0100

    logic and more styling

--- src/headerLogic.js
+++ src/headerLogic.js
@@ -1,10 +1,21 @@
 // selecting elements
-const headerIcons = document.querySelectorAll("header-Icons");
+const headerIcons = document.querySelectorAll("#header-Icons");
 
 // gelobal variables
+let iconCountChange = 0;
 
+console.log(headerIcons);
 // functions
-const changeIcons = () => {};
+const changeIcons = () => {
+  let iconChangeInterval = setInterval(() => {
+    if (headerIcons.length > iconCountChange) {
+      headerIcons[iconCountChange].classList.add("active");
+      iconCountChange++;
+    } else {
+      iconCountChange = 0;
+    }
+  }, 1000);
+};
 
 // events
 window.addEventListener("load", changeIcons);
