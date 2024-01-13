88eaa3363a17e7a204615b008858b8836a7d3a66
Ameer <ameerapex496@gmail.com>
2024-01-13 02:57:28 +0100

    selected elements needed

--- src/navbarLogic.js
+++ src/navbarLogic.js
@@ -1,4 +1,8 @@
 // selecting elements
+const navBtnBox = document.getElementsByClassName("nav-Btn-Box")[0];
+const navBtnLine = document.getElementsByClassName("nav-Btn-Line")[0];
+const navbarEl = document.getElementsByClassName("navbar")[0];
+const liElements = document.querySelectorAll("li");
 
 // gelobal variables
 
