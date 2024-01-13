6aea57c418296fb15057e0cc89d6db14fb28a053
Ameer <ameerapex496@gmail.com>
2024-01-13 06:59:02 +0100

    selected element + added function and event

--- src/headerLogic.js
+++ src/headerLogic.js
@@ -1,7 +1,10 @@
 // selecting elements
+const headerIcons = document.querySelectorAll("header-Icons");
 
 // gelobal variables
 
 // functions
+const changeIcons = () => {};
 
 // events
+window.addEventListener("load", changeIcons);
