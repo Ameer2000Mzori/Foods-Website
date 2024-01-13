8062a36163328953e85f1a814ac74e1a0568e20a
Ameer <ameerapex496@gmail.com>
2024-01-13 07:11:13 +0100

    finished the logic by adding icons one by one!

--- src/headerLogic.js
+++ src/headerLogic.js
@@ -9,13 +9,20 @@ console.log(headerIcons);
 const changeIcons = () => {
   let iconChangeInterval = setInterval(() => {
     if (headerIcons.length > iconCountChange) {
+      removeClasses();
       headerIcons[iconCountChange].classList.add("active");
       iconCountChange++;
     } else {
       iconCountChange = 0;
     }
-  }, 1000);
+  }, 2000);
 };
 
+// removing all active classes from the icons
+const removeClasses = () => {
+  headerIcons.forEach((headerIcon) => {
+    headerIcon.classList.remove("active");
+  });
+};
 // events
 window.addEventListener("load", changeIcons);
