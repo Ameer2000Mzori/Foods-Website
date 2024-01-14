730a09559b69ba7bc4d1877b5f2fb6c3b7706106
Ameer <ameerapex496@gmail.com>
2024-01-14 02:07:47 +0100

    selected elements needed + added array data for the menus

--- src/menuLogic.js
+++ src/menuLogic.js
@@ -1,6 +1,41 @@
 // selecting elements
+const menuBottomWrap = document.getElementsByClassName("menu-Bottom-Wrap")[0];
 
 // gelobal varibales
+
+// array of object data menu
+const arrayObjectData = [
+  {
+    title: "PANCAKES",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+  {
+    title: "CUPCAKES",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+  {
+    title: "HUMUS",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+  {
+    title: "HAMBURGER",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+  {
+    title: "SALMON",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+  {
+    title: "Vegetables",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
+  },
+];
 
 // classes
 
