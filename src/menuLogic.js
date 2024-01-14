e3f55c3134e94dc9f51811265769aaefdc719ad7
Ameer <ameerapex496@gmail.com>
2024-01-14 03:02:20 +0100

    bug fixes

--- src/menuLogic.js
+++ src/menuLogic.js
@@ -9,47 +9,41 @@ const arrayObjectData = [
     title: "PANCAKES",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-1.jpg",
   },
   {
     title: "CUPCAKES",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-2.jpg",
   },
   {
     title: "HUMUS",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-3.jpg",
   },
   {
     title: "HAMBURGER",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-4.jpg",
   },
   {
     title: "SALMON",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-5.jpg",
   },
   {
     title: "Vegetables",
     description:
       "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Autem qui ullam rerum, perferendis quisquam dignissimos omnis maxime, sequi totam temporibus",
-    img: "./assets/gallery-img-6.jpg",
   },
 ];
 
 // classes
 
-class showData {
-  constructeur(title, description, img) {
+class ShowData {
+  constructeur(title, description) {
     this.title = title;
     this.description = description;
-    this.img = img;
+    // this.img = img;
   }
 
   insertData() {
@@ -59,7 +53,7 @@ class showData {
     menuCardEl.innerHTML = `
     <img
     class="menu-Card-Imgs"
-    src="${this.img}"
+    src=""
     alt=""
   />
   <div class="menu-Card-Title-Wrap">
@@ -75,7 +69,9 @@ class showData {
 
 // functions
 arrayObjectData.forEach((menu) => {
-  let newShowData = new showData(menu.title, menu.description, menu.img);
+  console.log(menu.title, menu.description);
+  let newShowData = new ShowData(menu.title, menu.description);
+  console.log();
   newShowData.insertData();
 });
 // events
